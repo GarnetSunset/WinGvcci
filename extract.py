233a3b3ef50bdb6558c86880e23c5a72c4ffaf55
James <p1n3tr335@gmail.com>
2018-04-12 17:00:22 -0400

    I got floats baby

--- extract.py
+++ extract.py
@@ -1,4 +1,7 @@
 import colorgram, os
+from xml.dom import minidom
+
+mydoc = minidom.parse('schemes\WinGvcci.itermcolors')
 
 App = os.getenv('AppData')
 TranscodedPaper = (App + "\Microsoft\Windows\Themes\TranscodedWallpaper")
@@ -7,28 +10,47 @@ colors = colorgram.extract(TranscodedPaper, 7)
 
 first_color = colors[0]
 rgb1  = first_color.rgb
-print(rgb1)
+red1 = float(rgb1.r)/255
+green1 = float(rgb1.g)/255
+blue1 = float(rgb1.b)/255
 
 second_color = colors[1]
 rgb2  = second_color.rgb
-print(rgb2)
+red2 = float(rgb2.r)/255
+green2 = float(rgb2.g)/255
+blue2 = float(rgb2.b)/255
 
 third_color = colors[2]
 rgb3  = third_color.rgb
-print(rgb3)
+red3 = float(rgb3.r)/255
+green3 = float(rgb3.g)/255
+blue3 = float(rgb3.b)/255
 
 fourth_color = colors[3]
 rgb4  = fourth_color.rgb
-print(rgb4)
+red4 = float(rgb4.r)/255
+green4 = float(rgb4.g)/255
+blue4 = float(rgb4.b)/255
 
 fifth_color = colors[4]
 rgb5  = fifth_color.rgb
-print(rgb5)
+red5 = float(rgb5.r)/255
+green5 = float(rgb5.g)/255
+blue5 = float(rgb5.b)/255
 
 sixth_color = colors[5]
 rgb6  = sixth_color.rgb
-print(rgb6)
+red6 = float(rgb6.r)/255
+green6 = float(rgb6.g)/255
+blue6 = float(rgb6.b)/255
 
 seventh_color = colors[6]
 rgb7  = seventh_color.rgb
-print(rgb7)
+red7 = float(rgb7.r)/255
+green7 = float(rgb7.g)/255
+blue7 = float(rgb7.b)/255
+
+
+
+
+
